e93507a26a92c2855a341b849cbb6c7ddab9da20
Miacarmen <maitreyacarmen@gmail.com>
2024-04-23 19:31:17 -0700

    centered navbar, added subtitle on hero, fixed margins and padding

--- client/src/components/hero/Hero.js
+++ client/src/components/hero/Hero.js
@@ -165,89 +165,100 @@ const Hero = () => {
         // Desktop and Laptop Content
         <>
           <div className='main-container'>
-            <div className='hero-content grid grid-cols-2 bg-base-100 shadow-black'>
-              <div className='book-content text-secondary'>
+            <div className='hero-container bg-base-100 shadow-black'>
+              <div className='hero-header'>
                 <h2 className='cardTitle text-accent'>THE VOICE</h2>
-                <h3 className='subtitle'>Available Now on Amazon</h3>
-                <Link to='https://www.amazon.com/Voice-Journey-through-Eyes-Spirit/dp/0982578598/ref=sr_1_1?crid=1LK9ASSK76ZDP&keywords=the+voice+richard+carmen&qid=1706232501&s=books&sprefix=the+voice+richard+carmen%2Cstripbooks%2C141&sr=1-1'>
-                  <button className='btn shopBtn bg-accent mb-9'>
-                    BUY NOW
-                  </button>
-                </Link>
-
-                <div className='paragraph-container'>
-                  <p className='book-back pb-5'>
-                    Richard E. Carmen is a well-published Doctor of Audiology.
-                    After 40 years of silence he{"'"}s stepping forward with an
-                    astounding{' '}
-                    <span className='hero-highlight pl-1'>
-                      {' '}
-                      factual extraterrestrial contact and Divine intervention
-                    </span>{' '}
-                    story. Dr. Carmen had his first encounter when he was just a
-                    young boy. Thirty years later working as a research
-                    scientist he was determined to make his reconnection. He was
-                    even willing to give up everything to achieve it. He put his
-                    career on hold and began living like a monk in the city. Six
-                    months into his quest an extraterrestrial group reached out
-                    to him with an invitation to witness their presence. They
-                    set the date three months hence.
-                  </p>
-                  <p className='book-back pb-5'>
-                    During those 90 days, his family, friends, colleagues, even
-                    neighbors and their young children began witnessing
-                    astonishing contact events with him. Situations were clearly
-                    being manipulated in extraordinary ways. People began
-                    walking into his life. Nationally known Pastor Jack was
-                    convinced he was Godsent. Clinical psychologist Jo at first
-                    objectified his experiences until she herself became a
-                    witness. Medium Adele provided evidence of life after death.
-                    He met notable author, inventor and scientist Andrija
-                    Puharich, MD, who introduced him to a “star child” with
-                    unearthly abilities. Through prior extraterrestrial contact,
-                    Dr. Puharich had received knowledge of the New Physics that
-                    supports the science behind paranormal activity. In the
-                    meantime, Dr. Carmen had been receiving ongoing spiritual
-                    guidance from a mysterious and loving Voice.
-                  </p>
-
-                  <p className='book-back pb-5'>
-                    Advanced forms of life have been watching and waiting for
-                    the moment to make their global appearance. Many eyes and
-                    hearts are now being opened to this emerging reality. As
-                    more and more like-minded people share their truths, it is
-                    setting the stage for our long-hoped-for planetary
-                    awakening.
-                  </p>
-
-                  <p className='book-back'>
-                    All of these experiences transformed his understanding of
-                    religion, faith and God. He's come to realize that to bring
-                    change to the world, this family of Off-Planet Intelligence
-                    does not kill to conquer. They open hearts that enable us to
-                    see through the eyes of spirit.
-                  </p>
-                </div>
+                <h3 className='cardSubtitle'>
+                  Based on real people, actual events, and direct
+                  extraterrestrial contact
+                </h3>
               </div>
-
-              <div className='bookImgContainer pb-16'>
-                <figure className=' mobile-first'>
-                  <img
-                    src='https://i.imgur.com/h0RqrBG.png'
-                    alt='Book Cover'
-                    className='bookCoverImg'
-                  />
-
-                  <Link to='/artistbio'>
-                    <div className='artist-link pt-5'>
-                      Learn About the Artist Behind the Cover
-                    </div>
+              <div className='hero-content grid grid-cols-2 '>
+                <div className='book-content text-secondary'>
+                  {/* <h2 className='cardTitle text-accent'>THE VOICE</h2>
+                <h3 className='cardSubtitle'>Based on real people, actual events, and direct extraterrestrial contact</h3> */}
+                  <h3 className='subtitle'>Available Now on Amazon</h3>
+                  <Link to='https://www.amazon.com/Voice-Journey-through-Eyes-Spirit/dp/0982578598/ref=sr_1_1?crid=1LK9ASSK76ZDP&keywords=the+voice+richard+carmen&qid=1706232501&s=books&sprefix=the+voice+richard+carmen%2Cstripbooks%2C141&sr=1-1'>
+                    <button className='btn shopBtn bg-accent mb-9'>
+                      BUY NOW
+                    </button>
                   </Link>
 
-                  <Link to='/bookexcerpt'>
-                    <button className='btn preview-btn'>Book Excerpts</button>
-                  </Link>
-                </figure>
+                  <div className='paragraph-container'>
+                    <p className='book-back pb-5'>
+                      Richard E. Carmen is a well-published Doctor of Audiology.
+                      After 40 years of silence he{"'"}s stepping forward with
+                      an astounding{' '}
+                      <span className='hero-highlight pl-1'>
+                        {' '}
+                        factual extraterrestrial contact and Divine intervention
+                      </span>{' '}
+                      story. Dr. Carmen had his first encounter when he was just
+                      a young boy. Thirty years later working as a research
+                      scientist he was determined to make his reconnection. He
+                      was even willing to give up everything to achieve it. He
+                      put his career on hold and began living like a monk in the
+                      city. Six months into his quest an extraterrestrial group
+                      reached out to him with an invitation to witness their
+                      presence. They set the date three months hence.
+                    </p>
+                    <p className='book-back pb-5'>
+                      During those 90 days, his family, friends, colleagues,
+                      even neighbors and their young children began witnessing
+                      astonishing contact events with him. Situations were
+                      clearly being manipulated in extraordinary ways. People
+                      began walking into his life. Nationally known Pastor Jack
+                      was convinced he was Godsent. Clinical psychologist Jo at
+                      first objectified his experiences until she herself became
+                      a witness. Medium Adele provided evidence of life after
+                      death. He met notable author, inventor and scientist
+                      Andrija Puharich, MD, who introduced him to a “star child”
+                      with unearthly abilities. Through prior extraterrestrial
+                      contact, Dr. Puharich had received knowledge of the New
+                      Physics that supports the science behind paranormal
+                      activity. In the meantime, Dr. Carmen had been receiving
+                      ongoing spiritual guidance from a mysterious and loving
+                      Voice.
+                    </p>
+
+                    <p className='book-back pb-5'>
+                      Advanced forms of life have been watching and waiting for
+                      the moment to make their global appearance. Many eyes and
+                      hearts are now being opened to this emerging reality. As
+                      more and more like-minded people share their truths, it is
+                      setting the stage for our long-hoped-for planetary
+                      awakening.
+                    </p>
+
+                    <p className='book-back'>
+                      All of these experiences transformed his understanding of
+                      religion, faith and God. He's come to realize that to
+                      bring change to the world, this family of Off-Planet
+                      Intelligence does not kill to conquer. They open hearts
+                      that enable us to see through the eyes of spirit.
+                    </p>
+                  </div>
+                </div>
+
+                <div className='bookImgContainer pb-16'>
+                  <figure className=' mobile-first'>
+                    <img
+                      src='https://i.imgur.com/h0RqrBG.png'
+                      alt='Book Cover'
+                      className='bookCoverImg'
+                    />
+
+                    <Link to='/artistbio'>
+                      <div className='artist-link pt-5'>
+                        Learn About the Artist Behind the Cover
+                      </div>
+                    </Link>
+
+                    <Link to='/bookexcerpt'>
+                      <button className='btn preview-btn'>Book Excerpts</button>
+                    </Link>
+                  </figure>
+                </div>
               </div>
             </div>
           </div>
--- client/src/components/navigation/NavBar.js
+++ client/src/components/navigation/NavBar.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 // import { FaInstagram } from "react-icons/fa";
 // import { FaSquareFacebook } from "react-icons/fa6";
-
-
 
 const NavBar = () => {
   const ZIP_URL = 'https://auriclebooks.com/The-Voice-Media-Kit.zip';
@@ -21,32 +19,25 @@ const NavBar = () => {
 
   return (
     <div className='navbar'>
-      <div className='leftNav'>
-        <NavLink
-          to='/'
-          className={({ isActive }) =>
-            isActive ? 'navLink-active' : 'navLink'
-          }
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to='/about'
-          className={({ isActive }) =>
-            isActive ? 'navLink-active' : 'navLink'
-          }
-        >
-          About
-        </NavLink>
-        <NavLink
-          to='/author'
-          className={({ isActive }) =>
-            isActive ? 'navLink-active' : 'navLink'
-          }
-        >
-          Author
-        </NavLink>
-        {/* <NavLink
+      <NavLink
+        to='/'
+        className={({ isActive }) => (isActive ? 'navLink-active' : 'navLink')}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to='/about'
+        className={({ isActive }) => (isActive ? 'navLink-active' : 'navLink')}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to='/author'
+        className={({ isActive }) => (isActive ? 'navLink-active' : 'navLink')}
+      >
+        Author
+      </NavLink>
+      {/* <NavLink
           to='/otherbooks'
           id='other-books'
           className={({ isActive }) =>
@@ -55,39 +46,22 @@ const NavBar = () => {
         >
           Other&nbsp;Books
         </NavLink> */}
-        <NavLink
-          to='/testimonials'
-          id='testimonials'
-          className={({ isActive }) =>
-            isActive ? 'navLink-active' : 'navLink'
-          }
-        >
-          Testimonials
-        </NavLink>
-        <p className='navLink' id='downloadLink' onClick={handleDownload}>
-          Media Kit
-        </p>
-        <NavLink
-          to='/contact'
-          className={({ isActive }) =>
-            isActive ? 'navLink-active' : 'navLink'
-          }
-        >
-          Contact
-        </NavLink>
-      </div>
-      {/* <div className='rightNav'>
-        <div className='fb-icon'>
-        <Link to='https://www.facebook.com/auriclebooks/'>
-          <FaSquareFacebook />
-        </Link>
-        </div>
-        <div className='ig-icon'>
-        <Link to='https://www.instagram.com/auriclebooks/'>
-          <FaInstagram />
-        </Link>
-        </div>
-      </div> */}
+      <NavLink
+        to='/testimonials'
+        id='testimonials'
+        className={({ isActive }) => (isActive ? 'navLink-active' : 'navLink')}
+      >
+        Testimonials
+      </NavLink>
+      <p className='navLink' id='downloadLink' onClick={handleDownload}>
+        Media Kit
+      </p>
+      <NavLink
+        to='/contact'
+        className={({ isActive }) => (isActive ? 'navLink-active' : 'navLink')}
+      >
+        Contact
+      </NavLink>
     </div>
   );
 };
